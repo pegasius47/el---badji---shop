0dba671f334290faabcbddaac9a71fc3d030ad8c
pegasius47 <nabilbenyounes1998@gmail.com>
2025-10-21 01:12:45 +0100

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0b6eff',
+        gold: '#c79b2b'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif']
+      }
+    }
+  },
+  plugins: [],
+}
